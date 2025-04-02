70cd8265153947c20e349ca4542e4b325264e1f8
uno.yakshi <uno.yakshi@gmail.com>
2025-04-02 16:06:37 +0300

    progress: Solved functions2.rs; played with timings and threads...

--- exercises/02_functions/functions2.rs
+++ exercises/02_functions/functions2.rs
@@ -1,10 +1,33 @@
-// TODO: Add the missing type of the argument `num` after the colon `:`.
-fn call_me(num:) {
-    for i in 0..num {
-        println!("Ring! Call number {}", i + 1);
-    }
+use std::thread::{sleep, spawn};
+use std::time::{Duration, SystemTime, Instant};
+
+
+fn timeit<F: Fn() -> T, T>(f: F) -> T {
+    let start = Instant::now();
+    let result = f();
+    let duration = start.elapsed();
+    println!("It took {} milisecond(s).", duration.as_millis());
+    result
+}
+
+
+/// Enumerated and multithreaded println...
+fn call_me(lines_num: i64, threads_num: u32) {
+
+    // TODO: Calculate threads number if not defined...
+
+    // TODO: Run multiple threads...
+    let handle = spawn(move || {
+        for i in 1..lines_num {
+            let x = i * i;
+            println!("THREAD: {i} ==> {x}");
+        }
+    });
+    handle.join().unwrap();
+
+    println!("{threads_num} threads ran {lines_num} lines");
 }
 
 fn main() {
-    call_me(3);
+    timeit(|| call_me(1_000_000_0, 4));
 }
