f72ec4e9ce50b819369b14e40f0546283c85bd06
uno.yakshi <uno.yakshi@gmail.com>
2025-04-04 16:17:09 +0300

    progress: Solved 06_semantics...

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -1,9 +1,16 @@
+/// It takes the ownership!
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
     vec.push(88);
 
     vec
+}
+
+fn fill_vec_non_moving(vec_ref: &[i32]) -> Vec<i32> {
+    let mut new_vec = vec_ref.to_owned();
+    new_vec.push(88);
+    new_vec
 }
 
 fn main() {
@@ -14,15 +21,19 @@ fn main() {
 mod tests {
     use super::*;
 
-    // TODO: Make both vectors `vec0` and `vec1` accessible at the same time to
-    // fix the compiler error in the test.
     #[test]
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
-
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0.clone());
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
+
+        // Custom implementation...
+        let vec01 = vec![22, 44, 66];
+        let vec02 = fill_vec_non_moving(&vec01);
+
+        assert_eq!(vec01, [22, 44, 66]);
+        assert_eq!(vec02, [22, 44, 66, 88]);
     }
 }
